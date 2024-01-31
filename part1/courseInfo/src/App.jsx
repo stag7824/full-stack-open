d813dfe1aa5453aa2f37d4545ff25ffacbae1e36
sgardazi <stag7824@gmail.com>
2024-01-31 11:48:43 +0500

    exercise 1.1 and 1.2 starter code & ReadMe file addition

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+/* eslint-disable react/jsx-no-target-blank */
+const Header = (props) =>{
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>
+        {props.heading}
+      </h1>
+    </>
+  )
+}
+const Content = (props) =>{
+  return (
+    <>
+      <p>
+        {props.part} {props.exercises}
       </p>
+    </>
+  )
+}
+const Total = (props) =>{
+  return (
+    <>
+      <p>
+        Number of exercises {props.exercises1+props.exercises2+props.exercises3}
+      </p>
+    </>
+  )
+}
+
+
+const App = ()=>{
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return (
+    <>
+      <Header heading={course}/>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </>
   )
 }
