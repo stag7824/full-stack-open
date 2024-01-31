45674e3a6faf9da50e31d2e05afaa21bd17ce848
sgardazi <stag7824@gmail.com>
2024-01-31 12:13:33 +0500

    Exercise 1.1 and 1.2 Complete

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# FullStackOpen 2024
+# Full Stack Open 2024
 
-Solutions to exercises in FullStackOpen 2022
+Solutions to exercises in [Full Stack Open](https://fullstackopen.com/en) 2024
 
 ## Curriculum
 
-- [] Part 0: Fundamentals of Web apps
-- [x] Part 1: Introduction to React
-- [] Part 2: Communicating with Server
-- [] Part 3: Programming a server with NodeJS and Express
+- [ ] Part 0: Fundamentals of Web apps
+- [x] [Part 1: Introduction to React](./part1)
+- [ ] Part 2: Communicating with Server
+- [ ] Part 3: Programming a server with NodeJS and Express
 - [ ] Part 4: Testing Express servers, user administration
 - [ ] Part 5: Testing React apps
 - [ ] Part 6: State Management with Redux
--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -8,7 +8,8 @@ const Header = (props) =>{
     </>
   )
 }
-const Content = (props) =>{
+
+const Part = (props) =>{
   return (
     <>
       <p>
@@ -17,11 +18,20 @@ const Content = (props) =>{
     </>
   )
 }
+const Content = (props) =>{
+  return (
+    <>
+      <Part part={props.part[0]} exercises={props.exercises[0]}/>
+      <Part part={props.part[1]} exercises={props.exercises[1]}/>
+      <Part part={props.part[2]} exercises={props.exercises[2]}/>
+    </>
+  )
+}
 const Total = (props) =>{
   return (
     <>
       <p>
-        Number of exercises {props.exercises1+props.exercises2+props.exercises3}
+        Number of exercises {props.exercises[0]+props.exercises[1]+props.exercises[2]}
       </p>
     </>
   )
@@ -39,13 +49,8 @@ const App = ()=>{
   return (
     <>
       <Header heading={course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Content part={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]}/>
+      <Total exercises={[exercises1,exercises2,exercises3]} />
     </>
   )
 }
