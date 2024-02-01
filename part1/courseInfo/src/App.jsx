de3eb24172a75dee90724424035890f5b8ed22fa
sgardazi <stag7824@gmail.com>
2024-02-01 15:21:07 +0500

    Exercsie 1.5

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -3,7 +3,7 @@ const Header = (props) =>{
   return (
     <>
       <h1>
-        {props.heading}
+        {props.name}
       </h1>
     </>
   )
@@ -40,27 +40,29 @@ const Total = (props) =>{
 
 
 const App = ()=>{
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   
   return (
     <>
       <Header heading={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
     </>
   )
 }
